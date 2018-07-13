7d57aa79643df68394312aaed3cae38a8b7448a4
Kamakin Kirill <k.kamakin@protonmail.ch>
2018-07-13 13:42:17 +0300

    Provide support for multiline 'script ='

--- server/src/jsdomCaller.ts
+++ server/src/jsdomCaller.ts
@@ -20,32 +20,43 @@ function parseJsStatements(text: string): Statement[] {
         let line = lines[i];
         if (/^[ \t]*script/.test(line)) {
             let content: string;
-            let start: number;
-            if (match = /^[ \t]*script[ \t]*=(.+)$/m.exec(line)) {
-                content = match[1];
-                start = i;
+            let range: Range;
+            if (match = /(^[ \t]*script[ \t]*=[\s]*)(\S+[\s\S]*)$/m.exec(line)) {
+                content = match[2];
+                const matchStart = match[1].length
+                range = {
+                    start: { line: i, character: match[1].length },
+                    end: { line: i, character: matchStart + match[2].length }
+                };
+                let j = i;
+                while (++j < lines.length && !(/\bscript\b/.test(lines[j]) || /\bendscript\b/.test(lines[j])));
+                if (!(j === lines.length || /\bscript\b/.test(lines[j]))) {
+                    console.log("'script =' is multiline")
+                    while ((line = lines[++i]) !== undefined && !/\bendscript\b/.test(line)) content += line + '\n';
+                    range.end = { line: i - 1, character: lines[i - 1].length };
+                }
             } else {
-                start = i + 1;
+                range = {
+                    start: { line: i + 1, character: 0 },
+                    end: { line: i + 1, character: lines[i + 1].length }
+                };
                 content = "";
                 while ((line = lines[++i]) !== undefined && !/\bendscript\b/.test(line)) content += line + '\n';
+                range.end = { line: i - 1, character: lines[i - 1].length };
             }
             content = JSON.stringify(content);
             const statement = {
-                range: {
-                    start: { line: start, character: 0 },
-                    end: { line: i - 1, character: lines[i - 1].length }
-                },
-                declaration:
+                range: range, declaration:
                     `const proxy = new Proxy({}, {});\n` +
                     `const proxyFunction = new Proxy(new Function(), {});\n` +
                     `(new Function("widget","config","dialog", ${content}))\n` +
                     `.call(window, proxyFunction, proxy, proxy)`
             };
             result.push(statement);
-        } else if (match = /import[ \t]+(\S+)[ \t]*=.+/.exec(line)) {
+        } else if (match = /^[ \t]*import[ \t]+(\S+)[ \t]*=.+/.exec(line)) {
             importList += `"${match[1]}",`;
             importCounter++;
-        } else if (match = /(replace-value[ \t]*=[ \t]*)(\S+[ \t\S]*)$/.exec(line)) {
+        } else if (match = /(^[ \t]*replace-value[ \t]*=[ \t]*)(\S+[ \t\S]*)$/.exec(line)) {
             const content = stringifyStatement(match[2]);
             const matchStart = match.index + match[1].length;
             const statement = {
@@ -58,7 +69,7 @@ function parseJsStatements(text: string): Statement[] {
                     `.call(window, 5, 5, 5, 5)`
             };
             result.push(statement);
-        } else if (match = /(value[ \t]*=[ \t]*)(\S+[ \t\S]*)$/.exec(line)) {
+        } else if (match = /(^[ \t]*value[ \t]*=[ \t]*)(\S+[ \t\S]*)$/.exec(line)) {
             const content = stringifyStatement(match[2]);
             const call = generateCall(importCounter);
             const matchStart = match.index + match[1].length;
@@ -86,6 +97,22 @@ function parseJsStatements(text: string): Statement[] {
                     `proxyFunction, proxyFunction, proxyFunction, proxyFunction, proxyFunction, proxyFunction, \n` +
                     `proxyArray, proxyFunction, proxyFunction, proxyFunction${call})`
             };
+            result.push(statement);
+        } else if (match = /(^[ \t]*options[ \t]*=[ \t]*javascript:[ \t]*)(\S+[ \t\S]*)$/.exec(line)) {
+            const content = stringifyStatement(match[2]);
+            const matchStart = match[1].length;
+            const statement = {
+                range: {
+                    start: { line: i, character: matchStart },
+                    end: { line: i, character: matchStart + match[2].length }
+                }, declaration:
+                    `const proxyFunction = new Proxy(new Function(), {});\n` +
+                    `(new Function("requestMetricsSeriesValues","requestEntitiesMetricsValues",\n` +
+                    `"requestPropertiesValues","requestMetricsSeriesOptions","requestEntitiesMetricsOptions",\n` +
+                    `"requestPropertiesOptions", ${content}\n` +
+                    `)).call(window, proxyFunction, proxyFunction, proxyFunction, proxyFunction,\n` +
+                    ` proxyFunction, proxyFunction)`
+            }
             result.push(statement);
         }
     }
@@ -124,8 +151,7 @@ export function validate(document: TextDocument): Diagnostic[] {
         try {
             window.eval(statement.declaration);
         } catch (err) {
-            console.log(err.message);
-            if (/is not defined/.test(err.message)) result.push(Shared.createDiagnostic({
+            result.push(Shared.createDiagnostic({
                 uri: document.uri, range: statement.range
             }, DiagnosticSeverity.Warning, err.message
             ))
