2c5a8a40a20c54715f5a3b2d688b7920a08ef87e
Kamakin Kirill <k.kamakin@protonmail.ch>
2018-07-18 17:12:46 +0300

    Add grandparent test

--- server/src/test/required.test.ts
+++ server/src/test/required.test.ts
@@ -37,9 +37,23 @@ suite("Required settings for sections tests", () => {
 
     test("correct series with parent section", () => {
         const text =
-            "[configuration]\n" +
+            "[widget]\n" +
+            "   type = chart\n" +
             "   entity = hello\n" +
-            "\n" +
+            "   [series]\n" +
+            "       metric = hello\n";
+        const document = Shared.createDoc(text);
+        const expected: Diagnostic[] = [];
+        const result = Functions.lineByLine(document);
+        assert.deepEqual(result, expected);
+    });
+
+    test("correct series with grandparent section", () => {
+        const text =
+            "[group]\n" +
+            "   entity = hello\n" +
+            "[widget]\n" +
+            "   type = chart\n" +
             "   [series]\n" +
             "       metric = hello\n";
         const document = Shared.createDoc(text);
