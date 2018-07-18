0493d7bfcf8fb8319adf314225887a9ee3669fec
Kamakin Kirill <k.kamakin@protonmail.ch>
2018-07-18 18:11:53 +0300

    Improve test according to linter

--- server/src/test/sectionExtraText.test.ts
+++ server/src/test/sectionExtraText.test.ts
@@ -1,6 +1,6 @@
-import { TextDocument, TextEdit } from 'vscode-languageserver';
-import * as assert from 'assert';
-import * as Functions from '../formatFunctions';
+import * as assert from "assert";
+import { TextDocument, TextEdit } from "vscode-languageserver";
+import * as Functions from "../formatFunctions";
 
 function createDoc(text: string): TextDocument {
     return TextDocument.create("testDoc", "atsd-visual", 0, text);
@@ -9,10 +9,10 @@ function createDoc(text: string): TextDocument {
 suite("Extra text in section declaration line", () => {
 
     test("Correct declaration with previous text", () => {
-        const text = 
-            "some text\n" + 
+        const text =
+            "some text\n" +
             "\n" +
-            "[series]\n" + 
+            "[series]\n" +
             "   alias = s1";
         const document = createDoc(text);
         const expected: TextEdit[] = [];
@@ -21,8 +21,8 @@ suite("Extra text in section declaration line", () => {
     });
 
     test("Correct declaration without previous text", () => {
-        const text =  
-            "[series]\n" + 
+        const text =
+            "[series]\n" +
             "   alias = s1";
         const document = createDoc(text);
         const expected: TextEdit[] = [];
@@ -31,41 +31,42 @@ suite("Extra text in section declaration line", () => {
     });
 
     test("Incorrect declaration (text after)", () => {
-        const text =  
-            "[series]incorrect formatting\n" + 
+        const text =
+            "[series]incorrect formatting\n" +
             "   alias = s1";
         const document = createDoc(text);
         const expected: TextEdit[] = [{
             newText: "[series]\n\tincorrect formatting",
-            range: { start: {line: 0, character: 0 }, end: { line: 0, character: 28 } }
+            range: { start: { line: 0, character: 0 }, end: { line: 0, character: 28 } }
         }];
         const result = Functions.extraTextSectionLine(document);
         assert.deepEqual(result, expected);
     });
 
     test("Incorrect declaration (text before)", () => {
-        const text =  
-            "incorrect formatting[series]\n" + 
+        const text =
+            "incorrect formatting[series]\n" +
             "   alias = s1";
         const document = createDoc(text);
         const expected: TextEdit[] = [{
             newText: "incorrect formatting\n\n[series]",
-            range: { start: {line: 0, character: 0 }, end: { line: 0, character: 28 } }
+            range: { start: { line: 0, character: 0 }, end: { line: 0, character: 28 } }
         }];
         const result = Functions.extraTextSectionLine(document);
         assert.deepEqual(result, expected);
     });
 
     test("Incorrect declaration (text before and after)", () => {
-        const text =  
-            "incorrect formatting[series]incorrect formatting\n" + 
+        const text =
+            "incorrect formatting[series]incorrect formatting\n" +
             "   alias = s1";
         const document = createDoc(text);
         const expected: TextEdit[] = [{
             newText: "incorrect formatting\n\n[series]\n\tincorrect formatting",
-            range: { start: {line: 0, character: 0 }, end: { line: 0, character: 48 } }
+            range: { start: { line: 0, character: 0 }, end: { line: 0, character: 48 } }
         }];
         const result = Functions.extraTextSectionLine(document);
         assert.deepEqual(result, expected);
     });
+
 });
