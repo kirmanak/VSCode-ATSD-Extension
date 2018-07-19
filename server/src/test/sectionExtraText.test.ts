978893e8c9995964423ef3e55c0c0d5daa348941
codefactor-io <support@codefactor.io>
2018-07-19 15:18:22 +0000

    [CodeFactor] Apply fixes

--- server/src/test/sectionExtraText.test.ts
+++ server/src/test/sectionExtraText.test.ts
@@ -37,7 +37,7 @@ suite("Extra text in section declaration line", () => {
         const document = createDoc(text);
         const expected: TextEdit[] = [{
             newText: "[series]\n\tincorrect formatting",
-            range: { start: { line: 0, character: 0 }, end: { line: 0, character: 28 } }
+            range: { start: { line: 0, character: 0 }, end: { line: 0, character: 28 } },
         }];
         const result = Functions.extraTextSectionLine(document);
         assert.deepEqual(result, expected);
@@ -50,7 +50,7 @@ suite("Extra text in section declaration line", () => {
         const document = createDoc(text);
         const expected: TextEdit[] = [{
             newText: "incorrect formatting\n\n[series]",
-            range: { start: { line: 0, character: 0 }, end: { line: 0, character: 28 } }
+            range: { start: { line: 0, character: 0 }, end: { line: 0, character: 28 } },
         }];
         const result = Functions.extraTextSectionLine(document);
         assert.deepEqual(result, expected);
@@ -63,7 +63,7 @@ suite("Extra text in section declaration line", () => {
         const document = createDoc(text);
         const expected: TextEdit[] = [{
             newText: "incorrect formatting\n\n[series]\n\tincorrect formatting",
-            range: { start: { line: 0, character: 0 }, end: { line: 0, character: 48 } }
+            range: { start: { line: 0, character: 0 }, end: { line: 0, character: 48 } },
         }];
         const result = Functions.extraTextSectionLine(document);
         assert.deepEqual(result, expected);
